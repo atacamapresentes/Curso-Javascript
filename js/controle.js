2e0e2c3e48c713d9ae96b6b9473d015fc2033697
Guilherme Nastari <atacamapresentes@gmail.com>
2025-08-15 16:30:57 -0300

    Segundo commit

--- js/controle.js
+++ js/controle.js
@@ -1,25 +1,21 @@
 let main = document.getElementById('areaLista');
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
+let contador = 0;
+
+function addContador(){
+    contador++;
+}
 
 function addTarefa(){
     let valorInput = input.value;
 
     if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)){
-        let novoItem = `<div class="item">
-            <div class="item-icone">
+        let AddContador = contador++;
+
+        let novoItem = `<div id"contador" class="item">
+            <div id ="contador" class="item-icone">
                <i class="fa-regular fa-circle"></i>
-            </div>
-            <div class="item-nome">
-                Teste de Tarefa
-            </div>
-            <div class="item-botao">
-                <button class="delete"><i class="fa-solid fa-trash"></i>Deletar</button>
-            </div>
-        </div>
-        <div class="item clicado">
-            <div class="item-icone">
-               <i class="fa-solid fa-circle-check"></i>
             </div>
             <div class="item-nome">
                 Teste de Tarefa
@@ -29,5 +25,6 @@ function addTarefa(){
             </div>
         </div>`
     main.innerHTML += novoItem;
+    
     }    
-    }
+}
